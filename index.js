c38c72265675bc01c14503793b5446a4db870bf8
Nazire <nazirej@gmail.com>
2022-08-19 13:40:08 -0400

    commit tree 2b54843f666d280b9721a7886b756d813a7de55d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(arr) {
+    const newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+    }
+    return newArr;
+}
+
+function countDown(y) {
+    for (let i = y; i >= 0; i--) {
+        console.log(i)
+}
+
+}
